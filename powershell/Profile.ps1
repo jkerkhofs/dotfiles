c6e38dad96df199409575e2e3a0bd54139071e7f
Jo Kerkhofs <jo@kerkhofs.pro>
2022-01-31 21:59:34 +0100

    powershell: zoxide init

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -3,3 +3,8 @@ Set-Alias vim nvim
 
 # Setting the global node_path
 # $env:NODE_PATH = npm root -g
+
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
