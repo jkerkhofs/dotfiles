3065575e64f9416d473443925baf065c865b0081
Jo Kerkhofs <jo@kerkhofs.pro>
2022-01-31 15:42:23 +0100

    added mapping for telescope registers

--- vim/after/plugin/telescope.rc.lua
+++ vim/after/plugin/telescope.rc.lua
@@ -7,6 +7,7 @@ local actions = require('telescope.actions')
 
 vim.api.nvim_set_keymap('n', '<leader><space>', '<cmd>Telescope find_files<cr>', opts)
 vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>Telescope registers<cr>', opts)
 
 -- close telescope after first esc
 require('telescope').setup{
