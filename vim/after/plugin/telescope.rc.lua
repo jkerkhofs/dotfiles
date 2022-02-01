3f9d5dcb2004e7737556d69444c9a041d19cfb74
Jo Kerkhofs <jo@kerkhofs.pro>
2022-02-01 18:48:46 +0100

    added mapping for telescope live_grep and use git_files or find_files based on context

--- vim/after/plugin/telescope.rc.lua
+++ vim/after/plugin/telescope.rc.lua
@@ -2,22 +2,29 @@ if not vim.g.loaded_telescope then
   return
 end
 
-local opts = { noremap = true, silent = false }
+local opts = { noremap = true, silent = true }
 local actions = require('telescope.actions')
 
-vim.api.nvim_set_keymap('n', '<leader><space>', '<cmd>Telescope find_files<cr>', opts)
+vim.api.nvim_set_keymap("n", "<Leader><Space>", "<cmd>lua require'telescope-config'.project_files()<cr>", opts)
 vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>Telescope buffers<cr>', opts)
 vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>Telescope registers<cr>', opts)
+vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>Telescope live_grep<cr>', opts)
 
--- close telescope after first esc
 require('telescope').setup{
   defaults = {
-    file_ignore_patterns = {"node_modules"},
     mappings = {
       i = {
-        ["<esc>"] = actions.close,
-        ["<del>"] = actions.delete_buffer
+        ["<esc>"] = actions.close
       },
     },
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ["<del>"] = actions.delete_buffer
+        }
+      }
+    }
   }
 }
