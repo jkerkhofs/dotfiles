86d9281cf1012459dff49d2dc16e3a178e6fa840
Jo Kerkhofs <jo@kerkhofs.pro>
2022-06-08 07:30:02 +0200

    added helper functions to powershell profile

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -9,13 +9,20 @@ oh-my-posh init pwsh --config "$PSScriptRoot\pure.jke.omp.json" | Invoke-Express
 function SetCursorToBlock { Write-Host -NoNewLine "`e[2 q" }
 function SetCursorToLine { Write-Host -NoNewLine "`e[6 q" }
 function ResetCursor { SetCursorToLine }
-function SetGitProjectAsWindowTitle {
+function GetGitProjectName {
   $gitTopLevel = (git rev-parse --show-toplevel) 2> $null
+  $projectName = ""
   if ($gitTopLevel) {
-    $project = Split-Path (Resolve-Path $gitTopLevel) -Leaf
-    if ($project) {
-      $Host.UI.RawUI.WindowTitle = $project + " - " + $args
-    }
+    $projectName = Split-Path (Resolve-Path $gitTopLevel) -Leaf
+  }
+  return $projectName
+}
+function SetGitProjectAsWindowTitle {
+  $projectName = GetGitProjectName
+  if ($projectName) {
+    $suffix = ""
+    if ($args) { $suffix = " - " + $args }
+    $Host.UI.RawUI.WindowTitle = $projectName + $suffix
   }
 }
 
@@ -28,7 +35,7 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 Set-PsReadLineOption -EditMode Vi
 Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler {
-    if ($args[0] -eq 'Command') { SetCursorToBlock } else { SetCursorToLine }
+  if ($args[0] -eq 'Command') { SetCursorToBlock } else { SetCursorToLine }
 }
 
 
@@ -44,14 +51,30 @@ function vim {
 
 function la { ls -Force $args }
 
-# (e)xplorer
+# Explorer
 function e {
   if (!$args) { $args = Get-Location }
   explorer.exe $args
 }
 
-# (f)ind and open file
-function f { fzf | Invoke-Item }
+# File: find and open file
+function f { fzf --tac --header="Open file" | Invoke-Item }
+
+# Yank File Name: find and yank filename
+function yfn {
+  $path = ""
+  if ($args) { $path = Resolve-Path $args }
+  else { $path = fzf --tac --header="Yank filename" | Resolve-Path }
+  $path | Split-Path -Leaf | Set-Clipboard
+}
+
+# Yank File Path: find and yank filepath
+function yfp {
+  $path = ""
+  if ($args) { $path = Resolve-Path $args }
+  else { $path = fzf --tac --header="Yank filepath" | Resolve-Path }
+  $path | Set-Clipboard
+}
 
 # Open notes project in vim while auto pulling/pushing from git in the background every 5 min.
 # Cleans up background job after quiting vim.
@@ -79,6 +102,22 @@ function notes {
   cd $previousDir
 }
 
+# Open workspace with vim, lazygit, shell
+function work {
+  # open new tab
+  new-tab
+  # open new tab with lazygit
+  new-tab lazygit
+  # move focus back two tabs
+  wt -w 0 ft -p `; ft -p
+  vim
+}
+
+# New Tab
+function new-tab {
+  wt -w 0 nt -p PowerShell -d $pwd $args
+}
+
 
 # Setting the global node_path
 $env:NODE_PATH = npm root -g
@@ -88,4 +127,4 @@ $env:NODE_PATH = npm root -g
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
     (zoxide init --hook $hook powershell | Out-String)
-})
+  })
