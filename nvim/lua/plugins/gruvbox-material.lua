b161f3a712c0fd38a5d5d90f77cead04a062fe97
Jo Kerkhofs <jo@kerkhofs.pro>
2022-08-24 16:58:36 +0200

    refactor(vim): use GitGutter hlgroups for better theme support

--- nvim/lua/plugins/gruvbox-material.lua
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -1,3 +1,2 @@
 vim.g.gruvbox_material_transparent_background = 1
-vim.g.gruvbox_material_sign_column_background = 'none'
-vim.cmd([[colorscheme gruvbox-material]])
+vim.cmd 'colorscheme gruvbox-material'
