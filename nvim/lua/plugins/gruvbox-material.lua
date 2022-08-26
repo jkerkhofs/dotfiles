49c6e68cf07cb277e87c45642edcf81634fd045b
Jo Kerkhofs <jo@kerkhofs.pro>
2022-08-26 18:35:22 +0200

    feat(vim): added gruvbox_material settings

--- nvim/lua/plugins/gruvbox-material.lua
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -1,2 +1,4 @@
+vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_background = 'medium'
 vim.g.gruvbox_material_transparent_background = 1
 vim.cmd 'colorscheme gruvbox-material'
